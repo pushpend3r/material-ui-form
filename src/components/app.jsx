dcd8349b9c128e2933e6d269995f00751d3f567c
Pushpender Singh <piyushthakur5678@gmail.com>
2021-05-20 17:15:40 +0530

    frontend donw

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,141 +1,114 @@
-import React, { Component } from "react";
-import { Typography, AppBar, Toolbar, Box } from "@material-ui/core";
+import React from "react";
+import {
+  Typography,
+  Container,
+  Grid,
+  TextField,
+  Button,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-class App extends Component {
-  state = {};
-  render() {
-    return (
-      <>
-        <AppBar position="relative" color="default">
-          <Toolbar>
-            <Typography variant="h6" color="inherit" noWrap>
-              Company Name
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Box mt={2} mx="auto" px={2} maxWidth={700}>
-          <Typography variant="body1">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit optio
-            eos omnis repellendus iusto culpa corrupti modi corporis quaerat
-            deleniti officia aut, obcaecati eum veritatis sit? Laborum velit
-            autem nihil officia qui, consectetur dolores harum doloribus iste,
-            ipsum adipisci ducimus labore. Laboriosam dolorem dicta rem ullam.
-            Minima ea pariatur illo quidem repudiandae ad soluta distinctio.
-            Iusto autem pariatur libero ex non rem atque eligendi? Quaerat
-            eveniet perspiciatis itaque vel corrupti cupiditate voluptates?
-            Aperiam voluptatem sint fuga ratione iste quos odit? Rerum
-            recusandae quaerat reprehenderit repudiandae. Quis temporibus
-            exercitationem voluptas laudantium deserunt. Libero, expedita et
-            reiciendis voluptatibus amet mollitia? Dolorum molestias incidunt
-            eaque minus, nesciunt iusto eos ducimus vitae molestiae officia?
-            Modi ipsum dolorem quo non recusandae explicabo, quaerat labore sed
-            exercitationem, sequi dolore totam. Dolore ex officiis similique
-            neque aperiam repellat cupiditate, ratione aut ea reprehenderit
-            ducimus aliquam necessitatibus illo. Numquam, ipsa! Suscipit
-            perferendis minima eos dolorum, fugiat odit facere eligendi possimus
-            nam quaerat impedit, sit at! Quod asperiores, impedit quo totam
-            nihil illo facilis laboriosam eum ut repellendus laudantium animi
-            alias voluptatem beatae ex voluptas odit sequi neque vero aliquam
-            voluptate minus? Reprehenderit laborum aspernatur temporibus sed
-            error nihil distinctio iste dolor excepturi quo mollitia minima,
-            velit magni minus placeat earum tempore delectus fugit debitis et
-            quis provident odit nesciunt. Blanditiis error numquam cupiditate
-            veniam facere expedita laboriosam inventore harum, nemo a fuga non
-            dolor quaerat eveniet quam velit tempore id, modi nobis vitae
-            corrupti, recusandae corporis? At delectus earum beatae a iusto
-            rerum atque, cumque eum laborum repudiandae incidunt officiis hic
-            tempore itaque. Dolorum tempore velit modi inventore excepturi sequi
-            enim assumenda similique laborum, aspernatur fugit quo at molestiae
-            ipsa, natus dolores quasi debitis aliquam nemo libero praesentium!
-            Consequatur iste quisquam dolore neque quibusdam. Ut quo omnis
-            ratione tempora dolorum, nisi dolorem temporibus quidem. Ipsa dicta
-            omnis dolorum, rerum eum similique in quae possimus, voluptate
-            debitis labore, totam molestiae. Distinctio, error ullam, delectus
-            ex asperiores cumque nemo fuga veniam, nihil pariatur excepturi
-            doloremque laboriosam suscipit eum quidem nostrum. Animi quis in,
-            voluptatum soluta dolore excepturi temporibus minus voluptas unde.
-            Sequi cumque incidunt rem repellat, commodi illo vero, doloribus
-            voluptate ex, enim distinctio placeat officia sunt a! Distinctio ea
-            maxime sapiente perferendis autem quia impedit obcaecati animi
-            deleniti, dolorem quasi amet reprehenderit suscipit accusamus, optio
-            harum expedita explicabo. Non quisquam voluptatibus dolor iusto
-            eveniet eos a suscipit unde doloremque reiciendis sunt quos atque,
-            nemo blanditiis possimus quasi excepturi nulla veritatis minus
-            sapiente dolorum enim? Iusto, ad! Iusto rem modi, exercitationem
-            autem id optio. Optio labore velit deleniti? Enim natus optio, unde
-            quos eveniet accusamus provident totam. Modi esse, accusantium animi
-            harum temporibus at cum amet itaque laborum natus consectetur optio
-            culpa eligendi, fuga velit, rerum delectus quae blanditiis
-            praesentium perferendis! Officiis illo autem delectus commodi
-            excepturi totam quae sunt dolor. Itaque ab maxime fugit facere
-            veniam at nam natus esse ad libero architecto, velit blanditiis
-            quaerat saepe tenetur cupiditate reiciendis nihil? Pariatur facere,
-            maiores quas sunt accusamus quisquam labore! Incidunt recusandae
-            labore eius alias aspernatur amet quibusdam, corrupti eveniet, optio
-            facere vel voluptatibus itaque soluta autem eos dolorem molestias
-            sequi blanditiis cupiditate! Sapiente quos est pariatur,
-            voluptatibus asperiores quia minima! Distinctio eius deleniti amet
-            numquam excepturi reiciendis architecto repudiandae sequi autem
-            alias, delectus minima quasi veniam aut, rem harum blanditiis nam
-            reprehenderit pariatur aspernatur, consequuntur mollitia eos
-            eveniet. Temporibus voluptas fuga, commodi, iste assumenda cum vitae
-            culpa ullam accusamus officiis suscipit repudiandae consectetur
-            nobis reiciendis laudantium nesciunt debitis perspiciatis placeat.
-            Nesciunt in quae velit deserunt, fugit dolore, voluptatibus
-            voluptatem earum temporibus voluptatum obcaecati praesentium est!
-            Veniam mollitia a suscipit quam laudantium iusto corporis quisquam
-            repellat nesciunt, iure assumenda doloribus quidem perspiciatis, et
-            eaque, delectus consequuntur nisi nobis adipisci quo rem. Itaque
-            ipsum ullam eum adipisci, distinctio perspiciatis maiores placeat
-            odio? Iste fuga maxime hic inventore aperiam, optio nemo non
-            perspiciatis deserunt illum enim quis recusandae quam doloribus
-            rerum a voluptatem dolor labore eius at accusamus voluptas vero
-            neque? Corrupti nemo aut enim, laudantium quis animi vel itaque
-            aliquid eveniet quo, ipsam ex eum exercitationem? Dolorem similique
-            sapiente nemo at vel iusto commodi enim! Perferendis officia veniam,
-            magni repellendus, natus architecto voluptate harum eaque aspernatur
-            impedit, in blanditiis deleniti nostrum veritatis sint itaque. Ipsam
-            nisi labore ex, magnam necessitatibus similique in! Excepturi quo
-            cumque assumenda animi tenetur tempore totam saepe iusto expedita
-            sunt, labore obcaecati architecto distinctio, ea blanditiis magnam
-            dolores reprehenderit ut, ducimus porro. Possimus optio consequatur
-            obcaecati inventore libero eos, neque natus eveniet nam cum. Illum,
-            dolor beatae, reiciendis aperiam doloremque iure obcaecati itaque
-            neque, exercitationem voluptate ipsum quos consequuntur quis?
-            Explicabo quas nemo sunt quibusdam temporibus numquam molestiae
-            distinctio voluptatibus cum expedita quasi, inventore fugiat dolor
-            reiciendis consequatur. Eveniet corrupti odit deserunt quod
-            architecto cupiditate placeat magni eos ipsum natus vero praesentium
-            quos rem sed iure, ea totam odio in recusandae voluptatibus? In
-            harum explicabo alias officiis, sed sit reiciendis veritatis illo
-            hic eius? Fugiat alias tempore, aperiam quisquam, accusantium
-            pariatur illo totam laborum, veritatis nesciunt omnis! Voluptas
-            vero, cum saepe facilis blanditiis alias voluptates sequi delectus
-            ducimus non, doloribus qui est ut earum, libero hic? Alias quaerat
-            quia hic sapiente perspiciatis distinctio numquam beatae ab. Debitis
-            iste quo hic soluta itaque libero rem tempora voluptatibus! Aut nam
-            cupiditate reprehenderit dicta aliquam odit cumque enim expedita
-            fuga ut temporibus aliquid, voluptate dolorem earum perspiciatis
-            ducimus? Quasi nisi corporis veritatis consequatur fugit quibusdam
-            molestias optio repellat illo harum, praesentium magni sapiente
-            architecto repellendus autem earum. Quae ab odit ipsam modi amet
-            soluta. Culpa dicta, molestiae autem inventore, quibusdam sint iure
-            velit fuga expedita ipsa dignissimos vel, consequatur sunt! A
-            perspiciatis perferendis et ad mollitia illum, doloremque delectus
-            voluptatibus cum amet quasi voluptatum ullam tenetur quidem, itaque
-            quas repellendus culpa officiis quaerat, commodi rerum aperiam animi
-            est. Provident deserunt perferendis aperiam cupiditate ad,
-            voluptatibus obcaecati rerum soluta, itaque ut ea dolorem officia.
-            Reprehenderit facilis quisquam ad, eaque vero fuga pariatur suscipit
-            distinctio velit error unde harum delectus sapiente inventore
-            eveniet possimus cum voluptates perspiciatis provident magni tenetur
-            id nulla nam! Facilis reprehenderit maiores sint quam labore,
-            numquam totam temporibus!
-          </Typography>
-        </Box>
-      </>
-    );
-  }
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    marginTop: theme.spacing(3),
+  },
+}));
+
+function App() {
+  const styles = useStyles();
+  return (
+    <>
+      <Container maxWidth="sm" component="main" className={styles.paper}>
+        <Typography variant="h5" color="initial">
+          Get In Touch
+        </Typography>
+        <form className={styles.form}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs="12">
+              <TextField
+                name="email"
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+              />
+            </Grid>
+            <Grid item xs="12">
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="phoneno"
+                label="Phone No."
+                id="phoneno"
+              />
+            </Grid>
+            <Grid item xs="12">
+              <TextField
+                variant="outlined"
+                fullWidth
+                name="company"
+                label="Company"
+                id="company"
+              />
+            </Grid>
+            <Grid item xs="12">
+              <TextField
+                variant="outlined"
+                multiline
+                fullWidth
+                rows={4}
+                name="comment"
+                label="Comment"
+                id="comment"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={styles.submit}
+          >
+            Submit
+          </Button>
+        </form>
+      </Container>
+    </>
+  );
 }
 
 export default App;
